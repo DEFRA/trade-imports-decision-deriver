6eed5a9f4bb0a4bd82becb3b07a8d4da22c35f69
Thomas Anderson <t11omas1983@live.co.uk>
2025-04-14 09:40:25 +0100

    fixed test

--- tests/Deriver.Tests/Consumers/ImportPreNotificationConsumerTests.cs
+++ tests/Deriver.Tests/Consumers/ImportPreNotificationConsumerTests.cs
@@ -1,3 +1,5 @@
+using Btms.Business.Services.Decisions;
+using Defra.TradeImportsDataApi.Api.Client;
 using Defra.TradeImportsDecisionDeriver.Deriver.Consumers;
 using Defra.TradeImportsDecisionDeriver.TestFixtures;
 using Microsoft.Extensions.Logging.Abstractions;
@@ -9,11 +11,11 @@ public class ImportPreNotificationConsumerTests
     [Fact]
     public void OnHandle_ReturnsTaskCompleted()
     {
+        var apiClient = NSubstitute.Substitute.For<ITradeImportsDataApiClient>();
+        var decisionService = NSubstitute.Substitute.For<IDecisionService>();
         var consumer = new ImportPreNotificationConsumer(
             NullLogger<ImportPreNotificationConsumer>.Instance,
-            null!,
-            null!
-        );
+            decisionService, apiClient);
 
         var importNotification = ImportPreNotificationFixtures.ImportPreNotificationCreatedFixture();
 

fbcd449389303598fa0cf56a8a790102faaff6a9
Thomas Anderson <t11omas1983@live.co.uk>
2025-06-18 13:57:25 +0100

    updated CorrelationIdGenerator so it doesn't need to format the string

--- src/Deriver/Utils/CorrelationId/CorrelationIdGenerator.cs
+++ src/Deriver/Utils/CorrelationId/CorrelationIdGenerator.cs
@@ -5,7 +5,7 @@ public class CorrelationIdGenerator : ICorrelationIdGenerator
     public string Generate()
     {
         var timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
-        var random = Random.Shared.Next(1, 9999999);
-        return $"{timestamp}{random:0000000}";
+        var random = Random.Shared.Next(100000, 9999999);
+        return $"{timestamp}{random}";
     }
 }

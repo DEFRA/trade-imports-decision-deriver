bea5b5ac818789dded7d43e6240c22326664c7d8
t11omas <t11omas1983@live.co.uk>
2025-07-09 14:03:52 +0100

    added scenario to handle decision reason message when no documents are received (#103)

---
+++ tests/Deriver.Tests/Decisions/DecisionReasonBuilderTests.cs
@@ -0,0 +1,82 @@
+using Defra.TradeImportsDataApi.Domain.CustomsDeclaration;
+using Defra.TradeImportsDecisionDeriver.Deriver.Decisions;
+
+namespace Defra.TradeImportsDecisionDeriver.Deriver.Tests.Decisions;
+
+public class DecisionReasonBuilderTests
+{
+    // manual set
+    [Fact]
+    public void WhenDecisionReasonIsSetOnDecisionResult_ThenReasonShouldBeDecisionReason()
+    {
+        // Act
+        var result = DecisionReasonBuilder.Build(
+            new Commodity(),
+            new DocumentDecisionResult(null, "Test", 1, string.Empty, null, DecisionCode.X00, "Test Reason", null)
+        );
+
+        // Assert
+        result.Count.Should().Be(1);
+        result[0].Should().Be("Test Reason");
+    }
+
+    [Fact]
+    public void WhenDecisionResultIsNotLinkedToCheck_AndHasDocuments_ThenShouldBeNotLinkedReason()
+    {
+        // Act
+        var result = DecisionReasonBuilder.Build(
+            new Commodity()
+            {
+                Documents =
+                [
+                    new ImportDocument()
+                    {
+                        DocumentReference = new ImportDocumentReference("Test1234567"),
+                        DocumentCode = "C640",
+                    },
+                ],
+                Checks = [new CommodityCheck()],
+            },
+            new DocumentDecisionResult(null, "Test", 1, "Test Doc Ref", "H221", DecisionCode.X00, null, null)
+        );
+
+        // Assert
+        result.Count.Should().Be(1);
+        result[0]
+            .Should()
+            .Be(
+                "A Customs Declaration has been submitted however no matching CVEDA(s) have been submitted to Port Health (for CVEDA number(s) Test1234567). Please correct the CVEDA number(s) entered on your customs declaration."
+            );
+    }
+
+    [Fact]
+    public void WhenDecisionResultIsNotLinkedToChed_AndHasNoDocuments_AndCheckCodeIsH220_ThenShouldBeHmiGmsReason()
+    {
+        // Act
+        var result = DecisionReasonBuilder.Build(
+            new Commodity() { Checks = [new CommodityCheck() { CheckCode = "H220" }] },
+            new DocumentDecisionResult(null, "Test", 1, "Test Doc Ref", "H221", DecisionCode.X00, null, null)
+        );
+
+        // Assert
+        result.Count.Should().Be(1);
+        result[0]
+            .Should()
+            .Be(
+                "A Customs Declaration with a GMS product has been selected for HMI inspection. In IPAFFS create a CHEDPP and amend your licence to reference it. If a CHEDPP exists, amend your licence to reference it. Failure to do so will delay your Customs release."
+            );
+    }
+
+    [Fact]
+    public void WhenDecisionResultIsNotLinkedToChed_AndHasNotDocuments_AndCheckCodeIsNotH220_ThenShouldBeNoReason()
+    {
+        // Act
+        var result = DecisionReasonBuilder.Build(
+            new Commodity() { Checks = [new CommodityCheck() { CheckCode = "H221" }] },
+            new DocumentDecisionResult(null, "Test", 1, "Test Doc Ref", "H221", DecisionCode.X00, null, null)
+        );
+
+        // Assert
+        result.Count.Should().Be(0);
+    }
+}

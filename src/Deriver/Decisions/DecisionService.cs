c35b8bcbcaf845ad2adbda050ad27c6336586fcc
Thomas Anderson <t11omas1983@live.co.uk>
2025-08-27 09:36:17 +0100

    Updated missing ched decision reason text

--- src/Deriver/Decisions/DecisionService.cs
+++ src/Deriver/Decisions/DecisionService.cs
@@ -160,7 +160,7 @@ public class DecisionService(
             "H224" =>
                 "Customs declaration clearance withheld. Awaiting IUU check outcome. Contact Port Health Authority (imports) or Marine Management Organisation (landings).",
             _ =>
-                "This IPAFFS pre-notification reference cannot be found in IPAFFS. Please check that the reference is correct.",
+                "This CHED reference cannot be found in IPAFFS. Please check that the reference is correct.",
         };
         return reason;
     }

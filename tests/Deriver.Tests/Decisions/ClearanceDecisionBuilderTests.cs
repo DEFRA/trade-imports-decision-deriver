33d5f8d0f7e5ffb82bd5de0792c54968fba56ab0
Thomas Anderson <t11omas1983@live.co.uk>
2025-09-10 13:00:21 +0100

    added in extra tests

--- tests/Deriver.Tests/Decisions/ClearanceDecisionBuilderTests.cs
+++ tests/Deriver.Tests/Decisions/ClearanceDecisionBuilderTests.cs
@@ -159,4 +159,52 @@ public class ClearanceDecisionBuilderTests
         await Verify(clearanceDecision, _settings)
             .UseMethodName(nameof(BuildClearanceDecision_WithNoReasons_AndPreviousDecision));
     }
+
+    [Fact]
+    public async Task BuildClearanceDecision_WithNoReasons_ForE83()
+    {
+        var customsDeclaration = CustomsDeclarationResponseFixtures.CustomsDeclarationResponseFixture();
+        customsDeclaration.ClearanceRequest!.ExternalCorrelationId = "correlationId";
+        customsDeclaration.ClearanceDecision!.CorrelationId = "correlationId";
+        customsDeclaration.ClearanceRequest!.ExternalVersion = 22;
+        customsDeclaration.ClearanceDecision!.DecisionNumber = 4;
+        var decisionResult = new DecisionResult();
+        for (var i = 0; i < (customsDeclaration.ClearanceRequest?.Commodities!).Length; i++)
+        {
+            var commodity = (customsDeclaration.ClearanceRequest?.Commodities!)[i];
+            commodity.ItemNumber = i + 1;
+            commodity.Checks = commodity.Checks!.Take(1).ToArray();
+            commodity.Checks[0].CheckCode = "9115";
+
+            var documentReferenceCount = 1;
+            foreach (var document in commodity.Documents!)
+            {
+                document.DocumentCode = "9115";
+                document.DocumentReference!.Value = $"docref_{commodity.ItemNumber}_{documentReferenceCount}";
+                decisionResult.AddDecision(
+                    customsDeclaration.MovementReferenceNumber,
+                    commodity.ItemNumber!.Value!,
+                    document.DocumentReference!.Value,
+                    document.DocumentCode,
+                    commodity.Checks[0].CheckCode,
+                    DecisionCode.X00,
+                    internalDecisionCode: DecisionInternalFurtherDetail.E83
+                );
+
+                documentReferenceCount++;
+            }
+        }
+
+        var clearanceDecision = decisionResult.BuildClearanceDecision(
+            customsDeclaration.MovementReferenceNumber,
+            new CustomsDeclaration
+            {
+                ClearanceRequest = customsDeclaration.ClearanceRequest,
+                ClearanceDecision = customsDeclaration.ClearanceDecision,
+            },
+            new TestCorrelationIdGenerator("correlationId")
+        );
+
+        await Verify(clearanceDecision, _settings).UseMethodName(nameof(BuildClearanceDecision_WithNoReasons_ForE83));
+    }
 }

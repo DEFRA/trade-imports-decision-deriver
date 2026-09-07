56f73a7ed16a95a921d6617880c1df2e5e212336
Thomas Andoerson <t11omas1983@live.co.uk>
2026-09-07 12:42:32 +0100

    updated tests

--- tests/Deriver.Tests/Decisions/DecisionEngine/DecisionRules/Traces/TracesCvedaDecisionRuleTests.cs
+++ tests/Deriver.Tests/Decisions/DecisionEngine/DecisionRules/Traces/TracesCvedaDecisionRuleTests.cs
@@ -42,7 +42,7 @@ public class TracesCvedaDecisionRuleTests
         // Act
         var result = new TracesCvedaDecisionRule().Execute(
             context,
-            engineContext => new DecisionEngineResult(DecisionCode.C02, "Test")
+            engineContext => new DecisionEngineResult(DecisionCode.C03, "Test")
         );
 
         // Assert using FluentAssertions
